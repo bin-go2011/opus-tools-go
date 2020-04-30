e0eb6e0df3df781704370ffada2f3771feb340a6
Ben Ji <bin.go2011@yahoo.com>
2020-04-30 19:49:48 +0800

    move to openinfo_header.go

--- opusinfo/opusinfo_windows.go
+++ opusinfo/opusinfo_windows.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"ogg"
 	"syscall"
 	"unsafe"
 )
@@ -22,37 +21,6 @@ func init() {
 	}
 }
 
-type stream_processor struct {
-	process_page        func(*stream_processor, *ogg.Page)
-	process_end         func(*stream_processor)
-	isillegal           int32
-	constraint_violated int32
-	shownillegal        int32
-	isnew               int32
-	seqno               int32
-	lostseq             int32
-	seen_file_icons     int32
-
-	start int32
-	end   int32
-
-	num int
-	typ string
-
-	serial uint32 /* must be 32 bit unsigned */
-	os     ogg.StreamState
-	data   uintptr
-}
-
-type stream_set struct {
-	streams    []stream_processor
-	in_headers int32
-}
-
-func create_stream_set() *stream_set {
-	return &stream_set{}
-}
-
 func byteSliceToString(bval []byte) string {
 	for i := range bval {
 		if bval[i] == 0 {
@@ -68,18 +36,4 @@ func bytePtrToString(r uintptr) string {
 	}
 	bval := (*[1 << 10]byte)(unsafe.Pointer(r))
 	return byteSliceToString(bval[:])
-}
-
-func info_opus_start(stream *stream_processor) {
-	stream.typ = "opus"
-	stream.process_page = info_opus_process
-	stream.process_end = info_opus_end
-}
-
-func info_opus_process(stream *stream_processor, page *ogg.Page) {
-
-}
-
-func info_opus_end(stream *stream_processor) {
-
 }
