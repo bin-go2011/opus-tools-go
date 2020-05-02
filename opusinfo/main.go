5578849575584cb4594e5086a53300528c6bf55c
Ben Ji <bin.go2011@yahoo.com>
2020-05-03 00:34:36 +0800

    restruct

--- opusinfo/main.go
+++ opusinfo/main.go
@@ -7,42 +7,29 @@ import (
 	"path/filepath"
 )
 
-const CHUNK = 4500
-
-func info_opus_end(stream *stream_processor) {
-
-}
-
-func info_opus_process(stream *stream_processor, page *ogg.Page) {
-	var packet ogg.Packet
-	var h OpusHeader
-
-	streamState := &(stream.os)
-	streamState.Pagein(page)
-	streamState.Packetout(&packet)
-
-	opus_header_parse(packet.Data(), &h)
-}
-
-func info_opus_start(stream *stream_processor) {
-	stream.typ = "opus"
-	stream.process_page = info_opus_process
-	stream.process_end = info_opus_end
-}
-
-func create_stream_set() *stream_set {
-	return &stream_set{}
-}
-
 func find_stream_processor(set *stream_set, page *ogg.Page) *stream_processor {
 	var invalid int32
 	var constraint int32
 
 	serial := uint32(page.Serialno())
 
-	for _, stream := range set.streams {
+	for idx := range set.streams {
+		stream := set.streams[idx]
 		if serial == stream.serial {
+			set.in_headers = 0
 
+			if stream.end > 0 {
+				stream.isillegal = 1
+				stream.constraint_violated = CONSTRAINT_PAGE_AFTER_EOS
+				return &stream
+			}
+
+			stream.isnew = 0
+			stream.start = page.BeginningOfStream()
+			stream.end = page.EndOfStream()
+			stream.serial = serial
+
+			return &stream
 		}
 	}
 
@@ -70,7 +57,6 @@ func find_stream_processor(set *stream_set, page *ogg.Page) *stream_processor {
 			}
 		}
 
-		res = streamState.Packetout(&packet)
 		streamState.Clear()
 		streamState.Init(int(serial))
 	}
@@ -121,29 +107,33 @@ func process_file(name string) {
 	fmt.Printf("Processing file \"%s\"...\n\n", path)
 
 	ogsync := ogg.SyncState{}
-	page := ogg.Page{}
-
 	ogsync.Init()
 	defer ogsync.Clear()
 
-	written := 0
-	get_next_page(file, &ogsync, &page, &written)
+	page := ogg.Page{}
 
 	processors := create_stream_set()
-	p := find_stream_processor(processors, &page)
+	written := 0
 
-	if p.isnew > 0 {
-		fmt.Printf("New logical stream (#%d, serial: %08x): type %s\n",
-			p.num, p.serial, p.typ)
-	}
+	for i := 0; i < 2; i++ {
+		get_next_page(file, &ogsync, &page, &written)
 
-	if p.isillegal == 0 {
-		p.process_page(p, &page)
+		p := find_stream_processor(processors, &page)
 
-		if p.end > 0 {
-			p.process_end(p)
-			fmt.Printf("Logical stream %d ended\n", p.num)
+		if p.isnew > 0 {
+			fmt.Printf("New logical stream (#%d, serial: %08x): type %s\n",
+				p.num, p.serial, p.typ)
 		}
+
+		// if p.isillegal == 0 {
+		// 	p.process_page(p, &page)
+
+		// 	if p.end > 0 {
+		// 		p.process_end(p)
+		// 		fmt.Printf("Logical stream %d ended\n", p.num)
+		// 	}
+		// }
+
 	}
 }
 
--- opusinfo/opusinfo_header.go
+++ opusinfo/opusinfo_header.go
@@ -2,6 +2,35 @@ package main
 
 import "ogg"
 
+const (
+	CHUNK                      = 4500
+	CONSTRAINT_PAGE_AFTER_EOS  = 1
+	CONSTRAINT_MUXING_VIOLATED = 2
+)
+
+type misc_opus_info struct {
+	oh                   OpusHeader
+	bytes                int64
+	overhead_bytes       int64
+	lastlastgranulepos   int64
+	lastgranulepos       int64
+	firstgranule         int64
+	total_samples        int64
+	total_packets        int64
+	total_pages          int64
+	last_packet_duration int32
+	last_page_duration   int32
+	max_page_duration    int32
+	min_page_duration    int32
+	max_packet_duration  int32
+	min_packet_duration  int32
+	max_packet_bytes     int32
+	min_packet_bytes     int32
+	last_eos             int
+
+	doneheaders int
+}
+
 type stream_processor struct {
 	process_page        func(*stream_processor, *ogg.Page)
 	process_end         func(*stream_processor)
@@ -21,10 +50,14 @@ type stream_processor struct {
 
 	serial uint32 /* must be 32 bit unsigned */
 	os     ogg.StreamState
-	data   uintptr
+	data   *misc_opus_info
 }
 
 type stream_set struct {
 	streams    []stream_processor
 	in_headers int32
+}
+
+func create_stream_set() *stream_set {
+	return &stream_set{}
 }
