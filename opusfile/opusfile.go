71856cee45ebf293b987e399edab270695a73cb1
Ben Ji <bin.go2011@yahoo.com>
2020-05-05 00:39:14 +0800

    open opus file

--- opusfile/opusfile.go
+++ opusfile/opusfile.go
@@ -2,6 +2,7 @@ package opusfile
 
 import (
 	"ogg"
+	"os"
 )
 
 type OpusHead struct {
@@ -11,16 +12,25 @@ type OpusTags struct {
 }
 
 type OggOpusFile struct {
-	oy ogg.SyncState
+	oy     ogg.SyncState
+	stream *os.File
 }
 
 func (of *OggOpusFile) Close() {
 	of.oy.Clear()
+	of.stream.Close()
 }
 
-func Open(file string) *OggOpusFile {
-	of := &OggOpusFile{}
+func Open(file string) (*OggOpusFile, error) {
+	f, err := os.Open(file)
+	if err != nil {
+		return nil, err
+	}
+
+	of := &OggOpusFile{
+		stream: f,
+	}
 	of.oy.Init()
 
-	return of
+	return of, nil
 }
--- opusfile/opusfile_test.go
+++ opusfile/opusfile_test.go
@@ -5,6 +5,10 @@ import "testing"
 const SAMPLE_FILE = "../data/big_buck_bunny.opus"
 
 func TestOpenFile(t *testing.T) {
-	file := Open(SAMPLE_FILE)
+	file, err := Open(SAMPLE_FILE)
+	if err != nil {
+		t.Error(err)
+		return
+	}
 	defer file.Close()
 }
